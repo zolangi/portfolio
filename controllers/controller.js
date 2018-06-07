60c968365d2ddd32d123d48374822486e082e7f6
Zolangi Ramirez <zolangi@users.noreply.github.com>
2018-06-07 12:01:56 -0700

    Update controller.js

--- controllers/controller.js
+++ controllers/controller.js
@@ -4,7 +4,7 @@ myApp.controller('AppCtrl', ['$scope', '$http',
 function($scope, $http){
   var refresh = function(){
   $http.get('/resume').success(function($scope){
-      $scope.resume = {'school': 'California State University, Los Angeles', 'degree': 'Computer Science, B.S.', 'last_year': '2018'};
+      $scope.resume = [{'school': 'California State University, Los Angeles'}, {'degree': 'Computer Science, B.S.'}, {'last_year': '2018'}];
     });
 
   };
