01cf825b6355d5a4a38571fb395f5326125dfed0
Zolangi Ramirez <zolangi@users.noreply.github.com>
2018-06-07 11:59:42 -0700

    Update controller.js

--- controllers/controller.js
+++ controllers/controller.js
@@ -3,8 +3,8 @@ var myApp = angular.module('myApp', []);
 myApp.controller('AppCtrl', ['$scope', '$http',
 function($scope, $http){
   var refresh = function(){
-  $http.get('/resume').success(function(response){
-      $scope.resume = response;
+  $http.get('/resume').success(function($scope){
+      $scope.resume = {'school': 'California State University, Los Angeles', 'degree': 'Computer Science, B.S.', 'last_year': '2018'};
     });
 
   };
